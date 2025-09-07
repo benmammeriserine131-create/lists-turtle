0f220aa6a266b7ba23eeeaffffe915b43441d920
serine <benmammeriserine131@gmail.com>
2025-09-07 16:10:52 +0200

    Update import turtle.py

--- import turtle.py
+++ import turtle.py
@@ -3,7 +3,6 @@ t = turtle.Turtle()
 side_length = int(input("Enter the length: "))
 t.pensize(3)
 t.speed(5)
-t.showturtle()
 colors = ["red", "blue", "green", "orange", "purple", "black"]
 for i in range(6):
     t.color(colors[i])        
